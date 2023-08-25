07e157317a7fa6f4f61cd19566944fea2c1cd43b
Envoy666 <etherlord285@gmail.com>
2023-08-25 19:00:32 +0300

    first approximation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,17 @@
-import ru.gb.learn.toy.RealToyBox;
-import ru.gb.learn.toy.Toy;
-import ru.gb.learn.toy.ToyBox;
+import ru.gb.learn.toy.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Toy teddy = new Toy(0, "Teddy Bear", 1);
-        Toy robot = new Toy(1, "Terminator", 1);
-        Toy doll = new Toy(2, "Barbie", 1);
-        Toy jet = new Toy(3, "Jet", 1);
-        Toy ctor = new Toy(4, "Constructor", 1);
-        Toy sball = new Toy(5, "Soccer ball", 1);
+        DataProvider provider = new ToyListGenerator();
 
-        List<Toy> toyList = Arrays.asList(teddy, robot, doll, jet, ctor, sball);
+        List<Toy> toyList = provider.getToyList();
 
         ToyBox toyBox = new RealToyBox();
 
-        for (Toy toy : toyList) {
-            toyBox.put(toy);
-        }
+        toyBox.putAll(toyList);
 
         while (toyBox.isReady()) {
             System.out.println("toyBox.get() = " + toyBox.get());
--- src/main/java/ru/gb/learn/toy/ToyBox.java
+++ src/main/java/ru/gb/learn/toy/ToyBox.java
@@ -1,7 +1,11 @@
 package ru.gb.learn.toy;
 
+import java.util.List;
+
 public interface ToyBox {
-    void put(Toy toy);
+//    void put(Toy toy);
+
+    void putAll(List<Toy> toyList);
 
     Toy get();
 
